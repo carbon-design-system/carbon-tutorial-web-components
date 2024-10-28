26d70202418eef625b6675b7b58c61dce168c18b
Lee Chase <lee.chase@uk.ibm.com>
2024-10-28 19:43:52 +0000

    feat: a functional carbon button

--- main.js
+++ main.js
@@ -1,24 +1,18 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './style.scss';
+import '@carbon/web-components/es/components/button/button.js';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const bodyEl = document.querySelector('body');
 
-setupCounter(document.querySelector('#counter'))
+// button click handler
+const handleClick = () => {
+  bodyEl.classList.toggle('g10');
+  bodyEl.classList.toggle('g100');
+};
+document.querySelector('.button').addEventListener('click', handleClick);
+
+// set initial theme based on preferences
+if (matchMedia('prefers-color-scheme').matches) {
+  bodyEl.classList.add('g100');
+} else {
+  bodyEl.classList.add('g10');
+}
