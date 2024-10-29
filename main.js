ed2c55b16e9a167d32d021b2693c38551ad7f283
Lee Chase <lee.chase@uk.ibm.com>
2024-10-29 13:09:56 +0000

    feat: step 1 completed

--- main.js
+++ main.js
@@ -1,18 +1,52 @@
 import './style.scss';
 import '@carbon/web-components/es/components/button/button.js';
+import '@carbon/web-components/es/components/ui-shell/index';
+import '@carbon/web-components/es/components/checkbox/index';
+import '@carbon/web-components/es/components/content-switcher/index';
+import '@carbon/web-components/es/components/skip-to-content/index.js';
 
 const bodyEl = document.querySelector('body');
 
-// button click handler
-const handleClick = () => {
-  bodyEl.classList.toggle('g10');
-  bodyEl.classList.toggle('g100');
+const handleGlobalActionClick = (ev) => {
+  const targetPanelId = ev.currentTarget.getAttribute('panel-id');
+  const panels = document.querySelectorAll('cds-header-panel');
+  // check to see if other panels are open and close them
+  panels.forEach((panel) => {
+    if (panel.id !== targetPanelId) {
+      panel.expanded = false;
+    }
+  });
 };
-document.querySelector('.button').addEventListener('click', handleClick);
+const globalActions = document.querySelectorAll('cds-header-global-action');
+[...globalActions].forEach((action) =>
+  action.addEventListener('click', handleGlobalActionClick),
+);
 
-// set initial theme based on preferences
-if (matchMedia('prefers-color-scheme').matches) {
-  bodyEl.classList.add('g100');
-} else {
-  bodyEl.classList.add('g10');
-}
+const handleSwitch = (ev) => {
+  // Applies new theme or defers to system preferences by removing theme
+  switch (ev.detail.item.value) {
+    case 'light':
+      bodyEl.classList.remove('g100');
+      bodyEl.classList.add('g10');
+      break;
+    case 'dark':
+      bodyEl.classList.remove('g10');
+      bodyEl.classList.add('g100');
+      break;
+    default:
+      bodyEl.classList.remove('g10');
+      bodyEl.classList.remove('g100');
+  }
+};
+document
+  .querySelector('.theme-selector')
+  .addEventListener('cds-content-switcher-selected', handleSwitch);
+
+const handleHeaderCompliment = (ev) => {
+  document
+    .querySelector('cds-header')
+    .classList.toggle('compliment', ev.target.checked);
+};
+document
+  .querySelector('.theme-header__compliment')
+  .addEventListener('cds-checkbox-changed', handleHeaderCompliment);
