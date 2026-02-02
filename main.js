dbfdd79297a1c98f7469883ac2de81bcf36fe61c
Lee Chase <lee.chase@uk.ibm.com>
2026-02-02 11:20:34 +0000

    fix: use cds-grid and cds-column

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import '@carbon/web-components/es/components/ui-shell/index';
 import '@carbon/web-components/es/components/checkbox/index';
 import '@carbon/web-components/es/components/content-switcher/index';
 import '@carbon/web-components/es/components/skip-to-content/index.js';
+import '@carbon/web-components/es/components/grid/index';
 import '@carbon/web-components/es/components/breadcrumb/index';
 import '@carbon/web-components/es/components/tabs/index';
 const bodyEl = document.querySelector('body');
